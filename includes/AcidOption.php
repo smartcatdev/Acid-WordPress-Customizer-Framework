b143d6d722534bf2917b004ced0e8a3c849cb143
smartcatdev <bilal@smartcat.ca>
2017-11-07 10:18:14 -0500

    improved AcidOption class

--- includes/AcidOption.php
+++ includes/AcidOption.php
@@ -16,7 +16,7 @@ if( !class_exists( 'AcidOption' ) ) {
             'number',
             'hidden',
             'date',
-            
+            'image',
         );
         
         const TRANSPORT = 'refresh';
@@ -27,23 +27,13 @@ if( !class_exists( 'AcidOption' ) ) {
         
         private $option;
         
-        private $sanitize_callback;
-        
-        private $transport;
-        
-        private $acid_type;
-        
         private $setting_args = array();
         
         private $control_args = array();
         
-        /**
-         * 
-         * @param type $name
-         * @return String
-         */
+        
         public function __get( $name ) {
-            return $this->option[ $name ];
+            return isset( $this->option[ $name ] ) ? $this->option[ $name ] : false;
         }
         
         public function __construct( $section, $id, $option ) {
@@ -52,10 +42,7 @@ if( !class_exists( 'AcidOption' ) ) {
             $this->id = $id;
             $this->option = $option;
 
-
-            $this->set_type();
             $this->set_args();
-            $this->set_sanitization();
             $this->create_option();
             
             
@@ -71,29 +58,29 @@ if( !class_exists( 'AcidOption' ) ) {
             
         }
         
-
-        
         private function has_default() {
             return $this->default ? true : false;
         }
         
         private function has_transport() {
-            
+            return $this->transport ? true : false;
         }
         
         private function has_sanitize_callback() {
-            
+            return $this->sanitize_callback ? true : false;
         }
         
-        private function has_section() {
-            
-            if( ! $this->section ) {
-                _doing_it_wrong( 'AcidOption->has_section', __( 'You created an option without specifying the section' . $this->id, 'acid' ), '0.0.1' );
-            }
-            
+        private function has_label() {
+            return $this->label ? true : false;
         }
         
-        private function set_sanitization() {
+        private function has_type() {
+            return $this->type ? true : false;
+        }
+        
+        private function has_choices() {
+            
+            return $this->choices ? true : false;
             
         }
         
@@ -101,34 +88,16 @@ if( !class_exists( 'AcidOption' ) ) {
             
             global $wp_customize;
             
-            $setting_args = array(
-                
-                'default'               => $this->default ?: '',
-                'transport'             => $this->transport ?: self::TRANSPORT,
-                'sanitize_callback'     => 'sanitize_text_field',
-                
-            );
-            
-            $control_args = array(
-                
-                'type'                  => $this->type,
-                'section'               => $this->section,
-                'label'                 => $this->label,
-                
-            );
-            
-            
-            
             $wp_customize->add_setting( $this->id, $this->setting_args );
             $wp_customize->add_control( $this->id, $this->control_args );
-            
             
         }
         
         private function set_args() {
             
+            
             if( $this->has_sanitize_callback() ) {
-                $this->settings_args[ 'sanitize_callback' ] = $this->sanitize_callback;
+                $this->setting_args[ 'sanitize_callback' ] = $this->sanitize_callback;
             }else {
                 $this->set_sanitization();
             }
@@ -142,6 +111,73 @@ if( !class_exists( 'AcidOption' ) ) {
             }else {
                 $this->setting_args[ 'transport' ] = self::TRANSPORT;
             }
+            
+            if( $this->has_label() ) {
+                $this->control_args[ 'label' ] = $this->label;
+            }
+            
+            if( $this->has_choices() ) {
+                $this->control_args[ 'choices' ] = $this->choices;
+            }
+            
+            if( $this->has_type() ) {
+                $this->control_args[ 'type' ] = $this->type;
+            }
+            
+            $this->control_args[ 'section' ] = $this->section;
+            
+        }
+        
+        private function set_sanitization() {
+            
+            $callback = null;
+
+            switch( $this->type ) {
+                
+                case 'text' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'url' :
+                    $callback = 'esc_url_raw';
+                    break;
+                case 'number' :
+                    $callback = 'intval';
+                    break;
+                case 'textarea' :
+                    $callback = 'sanitize_textarea';
+                    break;
+                case 'date' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'checkbox' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'radio' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'select' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'dropdown-pages' :
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'email' : 
+                    $callback = 'sanitize_text_field';
+                    break;
+                case 'image' : 
+                    $callback = 'sanitize_text_field';
+                    break;
+                
+                
+                default :
+                    $callback = 'sanitize_text_field';
+                    break;
+                
+                
+                
+            }
+            
+            $this->setting_args[ 'sanitize_callback' ] = $callback;
             
         }
         
